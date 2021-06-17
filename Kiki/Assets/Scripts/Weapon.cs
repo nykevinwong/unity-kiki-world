3a79a59bcfc3ee832a9f58e77ff5acd0873a9e06
Kyang N Wong <nyckevinwong@gmail.com>
2021-06-17 14:11:08 -0400

    added player texture, texture pivot point, character rig, run & idle animation, weapon texture & animation, and weapon rotation with mouse cursor.

---
+++ Kiki/Assets/Scripts/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        // calculat angle based on direction and convert it to radian unit value from degree unit value
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; 
+        Quaternion rotation = Quaternion.AngleAxis(angle-90, Vector3.forward); // rotate angle value around z 
+        transform.rotation = rotation;
+    }
+}
